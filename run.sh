53019661a1cda625e0ace2a72ca1f4a84d51972d
gottfried <sascha.gottfried@googlemail.com>
2016-02-26 12:05:19 +0100

    ensure unique working directory using an uuid as a suffix

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 export LD_LIBRARY_PATH=../bin/LIB64
 export OMP_NUM_THREADS=1
 ulimit -s unlimited
-mkdir atrium_with_fans
-cp atrium_with_fans.fds atrium_with_fans
-cd atrium_with_fans
+WORK_DIR="atrium_with_fans-$(uuid)"
+mkdir $WORK_DIR
+cp atrium_with_fans.fds $WORK_DIR
+cd $WORK_DIR
 ../dist/openmpi_64/bin/mpiexec -np 4 ../bin/fds atrium_with_fans.fds &> stdout.log
